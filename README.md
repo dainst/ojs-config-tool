87f8de65ea9073e8d5155ae9964921d86408721f
paflov <philipp.franck@dainst.de>
2018-05-29 16:41:17 +0200

    automaticall create a journal

---
+++ README.md
@@ -0,0 +1,5 @@
+# ojs config tool
+A very small command line tool wich can be used by th docker contain for example to basically set up the ojs.
+
+Usage:
+php ojs-config-tool ojs2.php --path=[OJS2 installion path]
---
+++ ojs2.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+//*/
+
+$argv = isset($argv) ? $argv : array();
+$argv['path'] = !isset($argv['path']) ? '/var/www/html/ojs/' : realpath($argv['path']);
+
+if (!file_exists(realpath($argv['path'] . 'tools/bootstrap.inc.php'))) {
+    die("No OJS2 installation at '{$argv['path']}' found. Aborted.'\n");
+}
+
+require('tools/bootstrap.inc.php');
+
+class ojs_config_tool extends CommandLineTool {
+
+
+    function __construct($argv = array()) {
+        parent::CommandLineTool($argv);
+        $this->createJournal();
+    }
+
+    function createJournal($name='test', $path='test') {
+        $journal = New Journal();
+        $journal->setPath('test');
+        $journal->setEnabled(true);
+        $journal->setSetting('title', $name);
+        $journalDao = DAORegistry::getDAO('JournalDAO');
+        return $journalDao->insertJournal($journal);
+    }
+}
+
+$tool = new ojs_config_tool($argv);
+
+?>
